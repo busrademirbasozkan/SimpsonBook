03f1d11c185c912e38fe8562e9b271a02183e13e
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-11 18:47:23 +0300

    userinterface oluşturuldu.

---
+++ SimpsonBook/Controller/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  SimpsonBook
+//
+//  Created by Büşra Özkan on 11.11.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
